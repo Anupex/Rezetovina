aff8bd7ef121c22d3ba5ec7a3668f4b62528ca6f
Anupex <pecenka.j@centrum.cz>
2021-12-15 16:36:39 +0100

    funkcni s diakritikou

--- Rezetovina.py
+++ Rezetovina.py
@@ -18,11 +18,15 @@ def id_generator():
 
     seznam_id = []
 
-    file_vstup = open("vstup.txt", "r")
+    file_vstup = open("vstup.txt", "r+", encoding='utf-8')
+
     for vstup in file_vstup:
         jmeno = vstup.strip('\n').split()
         id_split = jmeno[1].lower() + '.' + jmeno[0][:2].lower()
         seznam_id.append(id_split)
+
+    for i in range(len(seznam_id)):
+       seznam_id[i] = unidecode(seznam_id[i])
     
     
     file_vstup.close()
@@ -33,9 +37,10 @@ def id_pwd_comb():
     file1 = open('data.txt', 'a+')
 
     for i in id_generator():
+        print('id: ' + i + ' pwd: ' + pwd_generator() + '\n')
         file1.write('id: ' + i + ' pwd: ' + pwd_generator() + '\n')
 
-    file1.close()
+    file1.close
 
 
 id_pwd_comb()
